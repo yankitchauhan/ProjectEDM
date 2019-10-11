cf8395f5b20896a85733b783d92f650be91c493a
Yankit Chauhan <yankitch@gmail.com>
2019-10-11 23:54:42 +0530

    Setup

---
+++ src/main/java/com/main/controller/HomeController.java
@@ -0,0 +1,36 @@
+package com.main.controller;
+
+import com.main.model.User;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.validation.annotation.Validated;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+import java.text.DateFormat;
+import java.util.Date;
+import java.util.Locale;
+
+@RequestMapping("")
+@Controller
+public class HomeController {
+    @RequestMapping(value = "/", method = RequestMethod.GET)
+    public String home(Locale locale, Model model) {
+        System.out.println("Home Page Requested, locale = " + locale);
+        Date date = new Date();
+        DateFormat dateFormat = DateFormat.getDateTimeInstance(DateFormat.LONG, DateFormat.LONG, locale);
+
+        String formattedDate = dateFormat.format(date);
+
+        model.addAttribute("serverTime", formattedDate);
+
+        return "home";
+    }
+
+    @RequestMapping(value = "/user", method = RequestMethod.POST)
+    public String user(@Validated User user, Model model) {
+        System.out.println("User Page Requested");
+        model.addAttribute("userName", user.getUserName());
+        return "user";
+    }
+}
---
+++ src/main/webapp/WEB-INF/views/home.jsp
@@ -0,0 +1,13 @@
+<%@ page contentType="text/html; charset=UTF-8" %>
+<html>
+<head>
+    <title> Project EDM </title>
+</head>
+<body>
+<P>The time on the server is ${serverTime}.</p>
+<form action="user" method="post">
+    <input type="text" name="userName"><br> <input
+        type="submit" value="Login">
+</form>
+</body>
+</html>
